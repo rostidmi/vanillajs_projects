bc72909e7a2ce0701232aea728f6ed597a41762d
unknown <udesurd@gmail.com>
2022-11-19 12:14:16 +0300

    upd

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -1,14 +1,80 @@
-const dicideArr = ["Rock", "Paper", "Scissors"];
+const gameArr = ["Rock!", "Paper!", "Scissors!"];
 const message = document.querySelector('#message');
-const btns = document.querySelectorAll("button");
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let playerScore = document.querySelector('#player-score')
+let computerScore = document.querySelector('#computer-score')
 
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", toss)
-}
+let score = [0, 0]
 
-function toss(e){
-    console.log(e.target.innerText);
-    let robot = Math.floor(Math.random()*2)
-    console.log("robot: " + dicideArr[robot])
-}
 
+rock?.addEventListener('click', () => {
+    let playerGuess = "Rock!";
+    let output;
+    let computer = Math.floor(Math.random() * 3)
+    let computerGuess = (gameArr[computer])
+    console.log(computerGuess)
+    if (playerGuess === computerGuess) {
+        output = "Tie! "
+
+    }
+    if (computerGuess == "Paper!") {
+
+        output = "Computer Wins!"
+        score[1]++
+        computerScore.innerHTML = score[1]
+    }
+    else {
+        output = "Player Wins!"
+        score[0]++
+        playerScore.innerHTML = score[0]
+    }
+    message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
+});
+paper?.addEventListener('click', () => {
+    let playerGuess = "Paper!";
+    let output;
+    let computer = Math.floor(Math.random() * 3)
+    let computerGuess = (gameArr[computer])
+    console.log(computerGuess)
+    if (playerGuess === computerGuess) {
+        output = "Tie! "
+
+    }
+    if (computerGuess == "Scissors!") {
+
+        output = "Computer Wins!"
+        score[1]++
+        computerScore.innerHTML = score[1]
+    }
+    else {
+        output = "Player Wins!"
+        score[0]++
+        playerScore.innerHTML = score[0]
+    }
+    message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
+});
+scissors?.addEventListener('click', () => {
+    let playerGuess = "Scissors!";
+    let output;
+    let computer = Math.floor(Math.random() * 3)
+    let computerGuess = (gameArr[computer])
+    console.log(computerGuess)
+    if (playerGuess === computerGuess) {
+        output = "Tie! "
+
+    }
+    if (computerGuess == "Rock!") {
+
+        output = "Computer Wins!"
+        score[1]++
+        computerScore.innerHTML = score[1]
+    }
+    else {
+        output = "Player Wins!"
+        score[0]++
+        playerScore.innerHTML = score[0]
+    }
+    message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
+});
