35bc71d7fb01000f23d23792477a6d1ba5fc507a
udesurd <udesurd@gmail.com>
2022-11-21 11:36:53 +0300

    done

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -5,8 +5,10 @@ const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 let playerScore = document.querySelector('#player-score')
 let computerScore = document.querySelector('#computer-score')
+let movesLeft = document.querySelector('#moves')
 
 let score = [0, 0]
+let moves = 10
 
 
 rock?.addEventListener('click', () => {
@@ -30,7 +32,17 @@ rock?.addEventListener('click', () => {
         score[0]++
         playerScore.innerHTML = score[0]
     }
+
+    moves--
+    movesLeft.innerText = moves
+
+    
+    
     message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
+
+    if(moves === 0 ){
+        gameOver()
+    }
 });
 paper?.addEventListener('click', () => {
     let playerGuess = "Paper!";
@@ -52,6 +64,12 @@ paper?.addEventListener('click', () => {
         output = "Player Wins!"
         score[0]++
         playerScore.innerHTML = score[0]
+    }
+
+    moves--
+    movesLeft.innerText = moves
+    if(moves === 0 ){
+        gameOver()
     }
     message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
 });
@@ -76,5 +94,32 @@ scissors?.addEventListener('click', () => {
         score[0]++
         playerScore.innerHTML = score[0]
     }
+
+    moves--
+    movesLeft.innerText = moves
+    if(moves === 0 ){
+        gameOver()
+    }
     message.innerHTML = `${output} </br> Player toss: ${playerGuess} Computer toss: ${computerGuess}`
 });
+
+function gameOver() {
+    const gameWindow = document.querySelector('.game')
+    const gameOverWindow = document.querySelector('.game-over-wrap')
+    const restartBtn = document.querySelector('#restart')
+    const result = document.querySelector('#result')
+   
+    gameWindow.style.display = 'none'
+    gameOverWindow.style.display = 'block'
+
+    if(score[0] > score [1]){
+        result.innerHTML = `Player Won! </br> ${score[0]} / ${score[1]}`
+    }
+    else if(score[0] < score [1]){
+        result.innerHTML = `Computer Won! </br> ${score[0]} / ${score[1]}`
+    }
+
+    restartBtn.addEventListener('click',() => {
+        window.location.reload();
+    })
+}
